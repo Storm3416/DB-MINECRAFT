5eebbe3b20084112154003977043033ae249413f
Storm3416 <90482804+Storm3416@users.noreply.github.com>
2024-08-27 19:34:29 +0200

    DB ADD
    d

---
+++ db.js
@@ -0,0 +1,93 @@
+/*
+    +---------------------------------------------------+
+    | ___ __ __   ______   ______    ______   _________ |
+    |/__//_//_/\ /_____/\ /_____/\  /_____/\ /________/\|
+    |\::\| \| \ \\:::_ \ \\:::_ \ \ \::::_\/_\__.::.__\/|
+    | \:.      \ \\:\ \ \ \\:(_) ) )_\:\/___/\  \::\ \  |
+    |  \:.\-/\  \ \\:\ \ \ \\: __ \ \ \_::._\:\  \::\ \ |
+    |   \. \  \  \ \\:\_\ \ \\ \ \ \ /  ____\: \  \::\ \|
+    |    \__\/ \__\/ \_____\/ \_\/ \_\/ \_______\  \__\/|
+    +---------------------------------------------------+
+                        By Storm3416
+*/
+
+import { world, system } from "@minecraft/server";
+let properties = []
+
+export class Database {
+    constructor(name) {
+        this.name = name;
+        const getDatabases = world.getDynamicPropertyIds()
+        if (getDatabases) {
+            getDatabases.forEach(p => {
+                const split = p.split(":")
+                if (split[0] === this.name) {
+                    if (!properties[this.name]) {
+                        properties[this.name] = {
+                            properties: [p]
+                        }
+                    } else {
+                        properties[this.name].properties.push(p)
+                    }
+                }
+            })
+        }
+
+        if (!properties[this.name]) {
+            properties[this.name] = {
+                properties: []
+            }
+        }
+
+        if (!properties[this.name].properties) {
+            properties[this.name] = {
+                properties: []
+            }
+        }
+
+    }
+    // property => string | number | boolean | Vector3
+    // value => string
+    set(property, value) {
+        world.setDynamicProperty(this.name + ":" + property, JSON.stringify(value))
+        if (properties[this.name].properties.find((c) => c.split(":")[1] == property)) return // console.warn(JSON.stringify(properties[this.name].properties))
+        properties[this.name].properties.push(this.name + ":" + property)
+        // console.warn(JSON.stringify(properties[this.name].properties))
+    }
+    // get => string
+    get(property) {
+        if (world.getDynamicProperty(this.name + ":" + property) === undefined) return undefined
+        return JSON.parse(world.getDynamicProperty(this.name + ":" + property))
+    }
+    // delete => string
+    delete(property) {
+        if (properties[this.name].properties.find((c) => c.split(":")[1] == property)) {
+            const find = properties[this.name].properties.find((c) => c.split(":")[1] == property)
+            const index = properties[this.name].properties.indexOf(find)
+
+            world.setDynamicProperty(properties[this.name].properties.find((c) => c.split(":")[1] == property))
+            properties[this.name].properties.splice(index, 1)
+            console.warn(JSON.stringify(properties[this.name].properties))
+        } else {
+            return console.warn('Propriety not found')
+        }
+    }
+    clearAll() {
+        const properties1 = properties[this.name].properties
+        properties1.forEach(p => {
+            world.getDynamicPropertyIds().forEach(d => {
+                if (p === d) {
+                    world.setDynamicProperty(d)
+                }
+            })
+        })
+        properties[this.name].properties = []
+    }
+    
+    clearAllProprieties() {
+        properties = []
+        world.getDynamicPropertyIds().forEach(d => {
+            world.setDynamicProperty(d)
+        })
+    }
+}
